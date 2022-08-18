8e6bc76f83ab49c55763642fc0a5b2dc657168bc
emanuel heredia <ema_359@hotmail.com>
2022-08-18 14:26:52 -0300

    configurando variables de entorno para url local y de produccion

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -30,7 +30,7 @@ const Formulario = ({ cliente, cargando }) => {
 			let respuesta;
 			if (cliente.id) {
 				//Editando Registro
-				const url = `http://localhost:3000/clientes/${cliente.id}`;
+				const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`;
 				respuesta = await fetch(url, {
 					method: "PUT",
 					body: JSON.stringify(datos),
@@ -40,7 +40,7 @@ const Formulario = ({ cliente, cargando }) => {
 				});
 			} else {
 				// Nuevo registro
-				const url = "http://localhost:3000/clientes";
+				const url = import.meta.env.VITE_API_URL;
 				respuesta = await fetch(url, {
 					method: "POST",
 					body: JSON.stringify(datos),
--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -6,7 +6,7 @@ const Inicio = () => {
 	useEffect(() => {
 		try {
 			const obtenerClientesAPI = async () => {
-				const url = "http://localhost:3000/clientes";
+				const url = import.meta.env.VITE_API_URL;
 				const respuesta = await fetch(url);
 				const resultado = await respuesta.json();
 				setClientes(resultado);
@@ -20,7 +20,7 @@ const Inicio = () => {
 		const confirmacion = confirm("¿Deseas eliminar este ciente?");
 		if (confirmacion) {
 			try {
-				const url = `http://localhost:3000/clientes/${id}`;
+				const url = `${import.meta.env.VITE_API_URL}/${id}`;
 				const respuesta = await fetch(url, {
 					method: "DELETE",
 				});
